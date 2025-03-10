8933bf8f768da6c3bd3c3ef53f7e9c704b888637
Borkata03 <A@DESKTOP-QD32O4N>
2025-03-11 00:08:50 +0200

    Main class implemented

--- LogScann/Program.cs
+++ LogScann/Program.cs
@@ -1,10 +1,54 @@
-﻿namespace LogScann
+﻿using CSoftt.Contract;
+using CSoftt.Contracts;
+using CSoftt.Services;
+using System;
+using System.Collections.Generic;
+
+public class Program
 {
-    internal class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string filePath = "logs.csv";
+
+        DateTime startDate = new DateTime(2025, 3, 1);
+        DateTime endDate = new DateTime(2025, 3, 7);
+
+
+        ILogReader logReader = new LogReader();
+        ILogProcessor logProcessor = new LogProcessor();
+
+
+        var logs = logReader.ReadLogFile(filePath);
+
+
+        var usersWithMultiplePages = logProcessor.GetUsersWithMultiplePages(logs, startDate, endDate);
+
+
+        Console.WriteLine("Users who have visited at least two different pages:");
+
+        using (StreamWriter writer = new StreamWriter("output.csv"))
         {
-            Console.WriteLine("Hello, World!");
+            writer.WriteLine("Users who have visited at least two different pages:");
+
+
+            foreach (var user in usersWithMultiplePages)
+            {
+                var userDate = logProcessor.GetUsersDateLogin(logs, user);
+                string formattedDate = userDate.ToString("dd-MM-yyyy, HH:mm") ?? "N/A";
+
+                string result = $"User: {user}, First Login: {formattedDate}";
+
+                Console.WriteLine(result);
+                writer.WriteLine(result);
+            }
         }
+
+        Console.WriteLine($"Results saved in: output.csv");
     }
+
+
+
+
+
+
 }
