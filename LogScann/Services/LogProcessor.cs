0bd0336af07ccf477410a28526484d7c7f6111d1
Borkata03 <A@DESKTOP-QD32O4N>
2025-03-11 00:08:27 +0200

    Services added and implemented

---
+++ LogScann/Services/LogProcessor.cs
@@ -0,0 +1,37 @@
+﻿using CSoftt.Contracts;
+using CSoftt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSoftt.Services
+{
+    public class LogProcessor : ILogProcessor
+    {
+        public DateTime GetUsersDateLogin(List<VisitLog> logs, string userId)
+        {
+            var dates = logs.Where(log => log.UserId == userId)
+                .Select(log => log.VisitDate)
+                .First();
+
+            return dates;  
+        }
+        List<string> ILogProcessor.GetUsersWithMultiplePages(List<VisitLog> logs, DateTime startDate, DateTime endDate)
+        {
+            
+            var filteredLogs = logs.Where(log => log.VisitDate >= startDate && log.VisitDate <= endDate).ToList();
+
+            
+            var result = filteredLogs
+                .GroupBy(log => log.UserId)
+                .Where(group => group.Select(log => log.PageId).Distinct().Count() >= 2)
+                .Select(group => group.Key)
+                .ToList();
+
+            return result;  
+        }
+    }
+}
---
+++ LogScann/Services/LogReader.cs
@@ -0,0 +1,35 @@
+﻿using CSoftt.Contract;
+using CSoftt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSoftt.Services
+{
+    public class LogReader : ILogReader
+    {
+        public List<VisitLog> ReadLogFile(string filePath)
+        {
+            var logs = new List<VisitLog>();
+
+            foreach (var line in File.ReadLines(filePath))
+            {
+                List<string> parts = line.Split(',').ToList();
+
+                if (parts.Count == 3)
+                {
+                    logs.Add(new VisitLog
+                    {
+                        VisitDate = DateTime.Parse(parts[0]),
+                        UserId = parts[1],
+                        PageId = parts[2]
+                    });
+                }
+            }
+
+            return logs;
+        }
+    }
+}
