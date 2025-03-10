e0a09f62da3afae78d7c75a6527d486f429d9562
Borkata03 <A@DESKTOP-QD32O4N>
2025-03-11 00:07:35 +0200

    ILogProcessor and ILogReader added

---
+++ LogScann/Contracts/ILogProcessor.cs
@@ -0,0 +1,17 @@
+﻿using CSoftt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSoftt.Contracts
+{
+    public interface ILogProcessor
+    {
+        List<string> GetUsersWithMultiplePages(List<VisitLog> logs, DateTime startDate, DateTime endDate);
+
+        DateTime GetUsersDateLogin(List<VisitLog> logs,string userId);
+    }
+}
+    
---
+++ LogScann/Contracts/ILogReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSoftt.Models;
+
+namespace CSoftt.Contract
+{
+    public interface ILogReader
+    {
+        List<VisitLog> ReadLogFile(string filePath);
+    }
+}
